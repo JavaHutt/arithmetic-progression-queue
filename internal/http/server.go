1cc18025ecf4a399ea0cd7f8a07bb9e959aba0bb
Alexander Karle <alxmer111@gmail.com>
2021-10-18 20:39:21 +0400

    swagger ui

--- internal/http/server.go
+++ internal/http/server.go
@@ -4,6 +4,9 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/JavaHutt/arithmetic-progression-queue/internal/model"
 	"github.com/go-chi/chi"
@@ -73,5 +76,28 @@ func (s *server) Handler() http.Handler {
 		r.Route("/tasks", newTasksHandler(s.encoder, s.taskService).Routes)
 	})
 
+	workDir, _ := os.Getwd()
+	swaggerDir := http.Dir(filepath.Join(workDir, "swaggerui"))
+	FileServer(r, "/swaggerui", swaggerDir)
+
 	return r
+}
+
+func FileServer(r chi.Router, path string, root http.FileSystem) {
+	if strings.ContainsAny(path, "{}*") {
+		panic("FileServer does not permit any URL parameters.")
+	}
+
+	if path != "/" && path[len(path)-1] != '/' {
+		r.Get(path, http.RedirectHandler(path+"/", http.StatusMovedPermanently).ServeHTTP)
+		path += "/"
+	}
+	path += "*"
+
+	r.Get(path, func(w http.ResponseWriter, r *http.Request) {
+		rctx := chi.RouteContext(r.Context())
+		pathPrefix := strings.TrimSuffix(rctx.RoutePattern(), "/*")
+		fs := http.StripPrefix(pathPrefix, http.FileServer(root))
+		fs.ServeHTTP(w, r)
+	})
 }
